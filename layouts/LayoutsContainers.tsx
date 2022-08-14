86e0de9709f69bab7678c12cc1e64e1087181751
mazenalhabeby <a.mazen.dessouky@gmail.com>
2022-08-14 12:21:20 +0200

    edit fb code

--- layouts/LayoutsContainers.tsx
+++ layouts/LayoutsContainers.tsx
@@ -1,4 +1,3 @@
-import {SidebarContextProvider} from '@/context/SidebarContext'
 import Head from 'next/head'
 import {useRouter} from 'next/router'
 import {useEffect} from 'react'
@@ -16,22 +15,6 @@ function LayoutsContainers({children}: ChildrenTypeProp) {
     <div>
       <Head>
         <title>LEAGUE OF PHARAOHS</title>
-        <meta
-          name="facebook-domain-verification"
-          content="u5h4aw86ylzoqssxtqqgz7hsq72rsj"
-        />
-        <script
-          type="application/javascript"
-          src="/components/scripts/FB.js"></script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            //@ts-ignore
-            style="display:none"
-            src="https://www.facebook.com/tr?id=654574859662239&ev=PageView&noscript=1"
-          />
-        </noscript>
       </Head>
       {children}
     </div>
---
+++ pages/_document.tsx
@@ -0,0 +1,55 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const originalRenderPage = ctx.renderPage
+
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component,
+      })
+
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="facebook-domain-verification"
+            content="u5h4aw86ylzoqssxtqqgz7hsq72rsj"
+          />
+          <script type="application/javascript" src="/scripts/fb.js"></script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{display: 'none'}}
+              src="https://www.facebook.com/tr?id=654574859662239&ev=PageView&noscript=1"
+            />
+          </noscript>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
