2a4d6fb67d15c3eb0a86c3eba3f7d2817cea4223
mazenalhabeby <a.mazen.dessouky@gmail.com>
2022-08-14 11:39:58 +0200

    edit the header

--- layouts/LayoutsContainers.tsx
+++ layouts/LayoutsContainers.tsx
@@ -16,6 +16,15 @@ function LayoutsContainers({children}: ChildrenTypeProp) {
     <div>
       <Head>
         <title>LEAGUE OF PHARAOHS</title>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WEPC74HGWB"></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-WEPC74HGWB');
+        </script>
       </Head>
       {children}
     </div>
