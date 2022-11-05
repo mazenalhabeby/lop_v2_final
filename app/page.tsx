95b248e22957d5dbc12baed7410205a21b11b815
mazenalhabeby <a.mazen.dessouky@gmail.com>
2022-11-05 10:24:01 +0100

    init

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-4xl text-green-500">this is a new area </h1>
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Test() {
+  return <div className="text-red-500 text-3xl">page</div>
+}
