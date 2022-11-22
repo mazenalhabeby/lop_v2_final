2e874216a211bae91b118bfd76f68faab1ef64b8
mazenalhabeby <a.mazen.dessouky@gmail.com>
2022-11-22 17:57:59 +0100

    add test page

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,7 +1,60 @@
-import React from "react"
+import {client} from "@/libs/sanity"
+import {getSession} from "next-auth/react"
+import {type} from "os"
+import React, {useState} from "react"
 
-const test = () => {
-  return <div>test</div>
+type resultType = {
+  _createdAt: String
+  _id: String
+  _rev: String
+  _type: String
+  _updatedAt: String
+  amount: String
+  coinPrice: number
+  coinUsed: String
+  seedRound: String
+  user: {
+    name: String
+  }
+  walletAddress: String
 }
 
-export default test
+const Test = () => {
+  const [result, setResult] = useState<resultType[]>([])
+  const query = `*[_type=="token-sale"] | order(walletAddress) {
+     _id,..., user->{name }
+  }`
+
+  const userAmount = client.fetch(query).then((data) => {
+    setResult(data)
+  })
+
+  return (
+    <div>
+      {result.map((item, i) => {
+        return (
+          <div key={i} className="grid grid-cols-12 justify-items-center py-1">
+            {/* <div className=" col-span-2">{item.user?.name}</div>
+            <div className=" col-span-1">{item.coinUsed}</div> */}
+            {/* <div className=" col-span-1">{item.coinPrice}</div> */}
+            <div className=" col-span-1">{item.amount}</div>
+            {/* <div className=" col-span-1">
+              {(Number(item.amount) * item.coinPrice).toLocaleString()}
+            </div>
+            <div className=" col-span-1">{item.seedRound}</div>
+            <div className=" col-span-1">
+              {item.seedRound == "preSale" ? 0.025 : 0.015}
+            </div>
+            <div className=" col-span-1">
+              {(Number(item.amount) * item.coinPrice) /
+                (item.seedRound == "preSale" ? 0.025 : 0.015)}
+            </div>
+            <div className=" col-span-3">{item.walletAddress}</div> */}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Test
