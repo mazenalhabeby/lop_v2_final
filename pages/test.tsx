99f86b30b77d072a5272e6deacc9e714bd3db369
mazenalhabeby <a.mazen.dessouky@gmail.com>
2025-12-19 09:53:24 +0100

    test page

---
+++ pages/test.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Test = ({utcTimeStamp}: any) => {
+  const timeStamp = new Date().toDateString()
+  const UTCtimeStamp = new Date(utcTimeStamp).toDateString()
+
+  console.log(UTCtimeStamp)
+  return (
+    <div>
+      <div>hell</div>
+      <div>{timeStamp}</div>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context: any) {
+  const utcTimeStamp = new Date().getTime()
+  return {
+    props: {utcTimeStamp},
+  }
+}
+
+export default Test
